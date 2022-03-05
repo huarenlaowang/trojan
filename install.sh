feb5030886ffc06ded5dfc41eb0f0ab04b932c57
huarenlaowang <97022835+huarenlaowang@users.noreply.github.com>
2022-03-06 01:50:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ REMOVE=0
 
 UPDATE=0
 
-DOWNLAOD_URL="https://github.com/Jrohy/trojan/releases/download/"
+DOWNLAOD_URL="https://github.com/huarenlaowang/trojan/releases/download/"
 
-VERSION_CHECK="https://api.github.com/repos/Jrohy/trojan/releases/latest"
+VERSION_CHECK="https://api.github.com/repos/huarenlaowang/trojan/releases/latest"
 
-SERVICE_URL="https://raw.githubusercontent.com/Jrohy/trojan/master/asset/trojan-web.service"
+SERVICE_URL="https://raw.githubusercontent.com/huarenlaowang/trojan/master/asset/trojan-web.service"
 
 [[ -e /var/lib/trojan-manager ]] && UPDATE=1
 
